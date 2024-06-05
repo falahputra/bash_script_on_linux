3adb0261228ca89914dc5202d9018dccfc7341ce
hasanulfalah <hasanul.junaidy@hashmicro.com.sg>
2024-06-05 22:39:21 +0800

    [3] Variables

---
+++ Class 03 - Variables/myscript.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#myname="Hasan"
+#myage="24"
+
+#echo "Hello, my name is $myname."
+#echo "I'm $myage years old."
+
+#word="fun"
+
+#echo "Linus is $word"
+#echo "Video games are $word"
+#echo "Sunny day are $word"
+
+#now=$(date)
+
+#echo "The system date and time is:  $now"
+
+name="Hasanul Falah Putra"
+now=$(date)
+
+echo "Hello $name"
+echo "The system date and time is:  $now"
+echo "Your username is : $USER"
